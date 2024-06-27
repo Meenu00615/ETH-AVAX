8f35e25f71d6a4371c23825db0c1631525909451
Meenu Patel <149779716+Meenu00615@users.noreply.github.com>
2024-06-27 18:20:50 +0530

    Update payable.sol

--- payable.sol
+++ payable.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.8.15;
 contract payableContract {
     address payable public owner;
-    constructo() {
+    constructor() {
     owner = payable(msg.sender);
     }
     function deposit () eternal payable{}
     function getBalance () external; view returns (uint){
         return address(this).balance;
+    }
+}
