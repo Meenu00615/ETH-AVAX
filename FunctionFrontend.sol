edbc0a5989da84b310734b09ed29ca3fe9567394
Meenu Patel <149779716+Meenu00615@users.noreply.github.com>
2024-06-28 11:55:02 +0530

    Create FunctionFrontend.sol

---
+++ FunctionFrontend.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+//import "hardhat/console.sol";
+
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+    }
+
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
+
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
+
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        balance += _amount;
+
+        assert(balance == _previousBalance + _amount);
+
+        //emit the event
+        emit Deposit(_amount);
+    }
+
+    //checking custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        //withdraw - given amount
+        balance -= _withdrawAmount;
+
+        //assert the balance is correct
+        assert(balance == (_previousBalance - _withdrawAmount));
+
+        //emit withdraw
+        emit Withdraw(_withdrawAmount);
+    }
+}
